151f2c6f92c3c313ac46037db9f99757b2b4283e
Yunxi_Zhu <zhu1265118991@gmail.com>
2026-03-23 16:50:22 +0800

    refactor: 删除有关 paddleOCR 和 deepseek-SSPU 模型的部分

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 # 加载环境变量
@@ -11,30 +12,9 @@ ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 # 测试文件目录
 TESTS_DIR = ROOT_DIR / "tests"
 
-
-# DeepseekSSPU 模型配置
-DEEPSEEK_SSPU_MODEL = {
-    "BASE_URL": os.getenv("DEEPSEEKSSPU_MODEL_BASE_URL"),
-    "MODEL_NAME": os.getenv("DEEPSEEKSSPU_MODEL_NAME"),
-    "TEMPERATURE": os.getenv("DEEPSEEKSSPU_MODEL_TEMPERATURE"),
-    "MAX_TOKENS": os.getenv("DEEPSEEKSSPU_MODEL_MAX_TOKENS"),
-}
-
 # qwen3-max 模型配置
 QWEN3_MAX_MODEL = {
     "MODEL_NAME": "qwen3-max",
     "API_KEY": os.getenv("QWEN3_MAX_API_KEY"),
-    "BASE_URL": "https://dashscope.aliyuncs.com/compatible-mode/v1"
-}
-
-
-# PaddleOCR-VL-1.5_API
-PaddleOCR_VL_1P5 = {
-    "API_URL": os.getenv("PADDLEOCR_VL_1P5_API_URL"),
-    "TOKEN": os.getenv("PADDLEOCR_VL_1P5_TOKEN")
-}
-
-# PaddleOCR-VL-1.5_SSPU_API
-PaddleOCR_VL_1P5_SSPU = {
-    "API_URL": os.getenv("PADDLEOCR_VL_1P5_SSPU_API_URL"),
+    "BASE_URL": "https://dashscope.aliyuncs.com/compatible-mode/v1",
 }
